6333de8fe2e0298eb9a67af1ac1e571231ee4813
Bruno da Silva Valenga <brunodasilvalenga@gmail.com>
2018-02-03 20:13:13 -0200

    create logic to use where options on get users

--- src/api/controllers/user.controller.js
+++ src/api/controllers/user.controller.js
@@ -1,27 +1,41 @@
-import _ from 'lodash'
 import httpStatus from 'http-status'
-import { handler as errorHandler } from '../middlewares/error'
 import models from '../../config/sequelize'
 
-const User = models.User
+export const list = async (req, res, next) => {
+  const { search } = req.query
 
-export const load = async (req, res, next, id) => {
   try {
-    const user = await User.get(id)
-    req.locals = { user }
-    return next()
-  } catch (error) {
-    return errorHandler(error, req, res)
+    const whereOptions = search && search.length > 1 ?
+      {
+        where: {
+          $or: [
+            {
+              firstName: {
+                $like: `%${search}%`
+              },
+            },
+            {
+              lastName: {
+                $like: `%${search}%`
+              }
+            }
+          ]
+        }
+      } : null
+    const options = {
+      attributes: { exclude: ['password'] }
+    }
+    const users = await models.User.findAll(Object.assign(options, whereOptions))
+    
+    res.status(200).json(users)
+  } catch (err) {
+    next(err)
   }
 }
 
-export const get = (req, res) => res.json(req.locals.user.transform())
-
-export const loggedIn = (req, res) => res.json(req.user.transform())
-
 export const create = async (req, res, next) => {
   try {
-    const user = await User.create(req.body)
+    const user = await models.User.create(req.body)
     const userTransformed = user.transform(user)
     return res.status(httpStatus.CREATED).json(userTransformed)
   } catch (err) {
@@ -30,42 +44,8 @@ export const create = async (req, res, next) => {
   }
 }
 
-export const replace = async (req, res, next) => {
-  try {
-    const { user } = req.locals
-    const newUser = new User(req.body)
-    const ommitRole = user.role !== 'admin' ? 'role' : ''
-    const newUserObject = _.omit(newUser.toObject(), '_id', ommitRole)
-
-    await user.update(newUserObject, { override: true, upsert: true })
-    const savedUser = await User.findById(user._id)
-
-    res.json(savedUser.transform())
-  } catch (error) {
-    next(User.checkDuplicateEmail(error))
-  }
-}
-
 export const update = (req, res, next) => {
-  const ommitRole = req.locals.user.role !== 'admin' ? 'role' : ''
-  const updatedUser = omit(req.body, ommitRole)
-  const user = Object.assign(req.locals.user, updatedUser)
-
-  user.save()
-    .then(savedUser => res.json(savedUser.transform()))
-    .catch(e => next(User.checkDuplicateEmail(e)))
-}
-
-export const list = async (req, res, next) => {
-  const { search } = req.query
-
-  try {
-    const users = search ? await User.findAll({ where: { $or: { firstName: { $like: `%${search}%` } }, lastName: { $like: `%${search}%` } } }) : await User.findAll()
-    const transformedUsers = users.map(user => user.transform(user))
-    res.json(transformedUsers)
-  } catch (err) {
-    next(err)
-  }
+  res.status(httpStatus.NO_CONTENT).end()
 }
 
 export const remove = (req, res, next) => {
