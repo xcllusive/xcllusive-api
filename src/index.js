507bb97712bd108ff61212ef575f74ec64716a96
Bruno da Silva Valenga <brunodasilvalenga@gmail.com>
2018-02-03 20:08:10 -0200

    remove bluebird

--- src/index.js
+++ src/index.js
@@ -1,16 +1,14 @@
-import 'bluebird' // eslint-disable-line no-global-assign
-
 import { port, env } from './config/vars'
 import app from './config/express'
 import models from './config/sequelize'
 
 models.sequelize.authenticate()
-  .then(() => models.sequelize.sync({}))
-  .then(() => {
-    app.listen(port, () => console.info(`server started on port ${port} (${env})`))
-  })
-  .catch((err) => {
-    console.error('Unable to connect to the database:', err)
-  })
+.then(() => models.sequelize.sync({}))
+.then(() => {
+  app.listen(port, () => console.info(`server started on port ${port} (${env})`))
+})
+.catch((err) => {
+  console.error('Unable to connect to the database:', err)
+})
 
 module.exports = app
