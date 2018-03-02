0865a4c6c854ec53d929ea3f0fa219310bcada2f
Cayo Bayestorff <cayobayestorff@Cayos-iMac.local>
2018-03-02 13:10:44 +1100

    Deleted many field in business table

--- src/api/models/business.model.js
+++ src/api/models/business.model.js
@@ -35,27 +35,11 @@ export default (sequelize, DataTypes) => {
       type: DataTypes.STRING,
       allowNull: true
     },
-    address1V: {
-      type: DataTypes.STRING,
-      allowNull: true
-    },
-    address2: {
-      type: DataTypes.STRING,
-      allowNull: true
-    },
-    address2V: {
-      type: DataTypes.STRING,
-      allowNull: true
-    },
     afterSalesNotes: {
       type: DataTypes.STRING,
       allowNull: true
     },
     businessABN: {
-      type: DataTypes.STRING,
-      allowNull: true
-    },
-    businessCategoryText: {
       type: DataTypes.STRING,
       allowNull: true
     },
@@ -72,10 +56,6 @@ export default (sequelize, DataTypes) => {
       allowNull: true
     },
     businessNameSecondary: {
-      type: DataTypes.STRING,
-      allowNull: true
-    },
-    businessNameSecondarySel: {
       type: DataTypes.STRING,
       allowNull: true
     },
@@ -115,10 +95,6 @@ export default (sequelize, DataTypes) => {
       type: DataTypes.STRING,
       allowNull: true
     },
-    countTaskDueNo: {
-      type: DataTypes.INTEGER,
-      allowNull: true
-    },
     createdBy: {
       type: DataTypes.STRING,
       allowNull: true
@@ -155,55 +131,11 @@ export default (sequelize, DataTypes) => {
       type: DataTypes.STRING,
       allowNull: true
     },
-    displayNameV: {
-      type: DataTypes.STRING,
-      allowNull: true
-    },
     firstNameV: {
       type: DataTypes.STRING,
       allowNull: false
     },
-    forSale: {
-      type: DataTypes.INTEGER,
-      allowNull: true
-    },
-    gDateFrom: {
-      type: DataTypes.DATE,
-      allowNull: true
-    },
-    gDateto: {
-      type: DataTypes.DATE,
-      allowNull: true
-    },
-    gGlobalPending: {
-      type: DataTypes.STRING,
-      allowNull: true
-    },
-    idReferrer: {
-      type: DataTypes.INTEGER,
-      allowNull: true
-    },
-    imNote: {
-      type: DataTypes.STRING,
-      allowNull: true
-    },
-    imRegenerate: {
-      type: DataTypes.INTEGER,
-      allowNull: true
-    },
-    imStatus: {
-      type: DataTypes.STRING,
-      allowNull: true
-    },
-    imVersion: {
-      type: DataTypes.INTEGER,
-      allowNull: true
-    },
     industry: {
-      type: DataTypes.STRING,
-      allowNull: true
-    },
-    introducerType: {
       type: DataTypes.STRING,
       allowNull: true
     },
@@ -215,23 +147,11 @@ export default (sequelize, DataTypes) => {
       type: DataTypes.STRING,
       allowNull: true
     },
-    listingAgentEmail: {
-      type: DataTypes.STRING,
-      allowNull: true
-    },
     location: {
       type: DataTypes.STRING,
       allowNull: true
     },
-    locationFormat: {
-      type: DataTypes.STRING,
-      allowNull: true
-    },
     lostDate: {
-      type: DataTypes.DATE,
-      allowNull: true
-    },
-    lostDateCopy: {
       type: DataTypes.DATE,
       allowNull: true
     },
@@ -247,10 +167,6 @@ export default (sequelize, DataTypes) => {
       type: DataTypes.DATE,
       allowNull: true
     },
-    notifyOwner: {
-      type: DataTypes.STRING,
-      allowNull: true
-    },
     ownersTime: {
       type: DataTypes.STRING,
       allowNull: true
@@ -259,103 +175,11 @@ export default (sequelize, DataTypes) => {
       type: DataTypes.INTEGER,
       allowNull: true
     },
-    postCodeV: {
-      type: DataTypes.INTEGER,
-      allowNull: true
-    },
-    potentialListing: {
-      type: DataTypes.INTEGER,
-      allowNull: true
-    },
     recID: {
       type: DataTypes.INTEGER,
       allowNull: true
     },
-    referralList: {
-      type: DataTypes.STRING,
-      allowNull: true
-    },
-    referrerComAgreed: {
-      type: DataTypes.INTEGER,
-      allowNull: true
-    },
-    referrerComSold: {
-      type: DataTypes.INTEGER,
-      allowNull: true
-    },
-    saleNotesLostMeeting: {
-      type: DataTypes.STRING,
-      allowNull: true
-    },
-    saleNotesLostWant: {
-      type: DataTypes.STRING,
-      allowNull: true
-    },
-    saleNotesReasonChoose: {
-      type: DataTypes.STRING,
-      allowNull: true
-    },
-    saleNotesReasonType: {
-      type: DataTypes.STRING,
-      allowNull: true
-    },
-    searchNote: {
-      type: DataTypes.STRING,
-      allowNull: true
-    },
-    sellabilityCounter: {
-      type: DataTypes.INTEGER,
-      allowNull: true
-    },
-    sellabilityLockout: {
-      type: DataTypes.INTEGER,
-      allowNull: true
-    },
-    sellabilityStatus: {
-      type: DataTypes.STRING,
-      allowNull: true
-    },
-    settlementDate: {
-      type: DataTypes.DATE,
-      allowNull: true
-    },
-    sold: {
-      type: DataTypes.INTEGER,
-      allowNull: true
-    },
-    soldAgentCalc: {
-      type: DataTypes.INTEGER,
-      allowNull: true
-    },
-    soldCalc: {
-      type: DataTypes.INTEGER,
-      allowNull: true
-    },
-    soldCalcDisplay: {
-      type: DataTypes.STRING,
-      allowNull: true
-    },
-    soldDataCheck: {
-      type: DataTypes.INTEGER,
-      allowNull: true
-    },
-    soldOwnerCalc: {
-      type: DataTypes.INTEGER,
-      allowNull: true
-    },
-    soldPrice: {
-      type: DataTypes.INTEGER,
-      allowNull: true
-    },
-    soldStockPrice: {
-      type: DataTypes.INTEGER,
-      allowNull: true
-    },
     sort: {
-      type: DataTypes.STRING,
-      allowNull: true
-    },
-    sortBy: {
       type: DataTypes.STRING,
       allowNull: true
     },
@@ -375,29 +199,13 @@ export default (sequelize, DataTypes) => {
       type: DataTypes.STRING,
       allowNull: true
     },
-    statsLeadNurtureStatus: {
-      type: DataTypes.STRING,
-      allowNull: true
-    },
     suburb: {
-      type: DataTypes.STRING,
-      allowNull: true
-    },
-    suburbV: {
       type: DataTypes.STRING,
       allowNull: true
     },
     lastNameV: {
       type: DataTypes.STRING,
       allowNull: false
-    },
-    underOffer: {
-      type: DataTypes.INTEGER,
-      allowNull: true
-    },
-    uVendorPhone1: {
-      type: DataTypes.INTEGER,
-      allowNull: true
     },
     vendorEmail: {
       type: DataTypes.STRING,
@@ -407,16 +215,8 @@ export default (sequelize, DataTypes) => {
       type: DataTypes.STRING,
       allowNull: true
     },
-    vendorFaxAC: {
-      type: DataTypes.INTEGER,
-      allowNull: true
-    },
     vendorPhone1: {
       type: DataTypes.STRING,
-      allowNull: true
-    },
-    vendorPhone1AC: {
-      type: DataTypes.INTEGER,
       allowNull: true
     },
     vendorPhone2: {
@@ -424,22 +224,6 @@ export default (sequelize, DataTypes) => {
       allowNull: true
     },
     vendorPhone3: {
-      type: DataTypes.STRING,
-      allowNull: true
-    },
-    wCreatedBy: {
-      type: DataTypes.STRING,
-      allowNull: true
-    },
-    withdrawn: {
-      type: DataTypes.INTEGER,
-      allowNull: true
-    },
-    withdrawnCalc: {
-      type: DataTypes.INTEGER,
-      allowNull: true
-    },
-    wModifiedBy: {
       type: DataTypes.STRING,
       allowNull: true
     }
