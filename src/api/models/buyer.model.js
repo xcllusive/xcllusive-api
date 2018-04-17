e0d87a7c0de87e2b7a236594bd3be85fbf0bcdab
Bruno da Silva Valenga <brunodasilvalenga@gmail.com>
2018-04-16 22:46:20 -0300

    include prettier settings ; set model buyer to only allowNull values

--- src/api/models/buyer.model.js
+++ src/api/models/buyer.model.js
@@ -1,158 +1,162 @@
 export default (sequelize, DataTypes) => {
-  const Buyer = sequelize.define('Buyer', {
-    id: {
-      type: DataTypes.INTEGER,
-      autoIncrement: true,
-      primaryKey: true,
-      unique: true,
-      allowNull: true
-    },
-    region: {
-      type: DataTypes.STRING,
-      allowNull: true
-    },
-    buyerNotes: {
-      type: DataTypes.STRING,
-      allowNull: true
-    },
-    smSent: {
-      type: DataTypes.STRING,
-      allowNull: true
-    },
-    smSentDate: {
-      type: DataTypes.DATE,
-      allowNull: true
-    },
-    active: {
-      type: DataTypes.STRING,
-      allowNull: true
-    },
-    address2: {
-      type: DataTypes.STRING,
-      allowNull: true
-    },
-    businessIndustry: {
-      type: DataTypes.STRING,
-      allowNull: true
-    },
-    businessType: {
-      type: DataTypes.STRING,
-      allowNull: true
-    },
-    buyerSource: {
-      type: DataTypes.STRING,
-      allowNull: true
-    },
-    caReceived: {
-      type: DataTypes.STRING,
-      allowNull: true
-    },
-    caSent: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    checkAddress: {
-      type: DataTypes.STRING,
-      allowNull: true
-    },
-    checkEmail: {
-      type: DataTypes.STRING,
-      allowNull: true
-    },
-    country: {
-      type: DataTypes.STRING,
-      allowNull: true
-    },
-    displayName: {
-      type: DataTypes.STRING,
-      allowNull: true
-    },
-    email: {
-      type: DataTypes.STRING,
-      allowNull: true
-    },
-    emailOptional: {
-      type: DataTypes.STRING,
-      allowNull: true
-    },
-    fax: {
-      type: DataTypes.INTEGER,
-      allowNull: true
-    },
-    firstName: {
-      type: DataTypes.STRING,
-      allowNull: true
-    },
-    mailingAddress: {
-      type: DataTypes.INTEGER,
-      allowNull: true
-    },
-    newsletter: {
-      type: DataTypes.STRING,
-      allowNull: true
-    },
-    ownersTime: {
-      type: DataTypes.STRING,
-      allowNull: true
-    },
-    postCode: {
-      type: DataTypes.INTEGER,
-      allowNull: true
-    },
-    priceFrom: {
-      type: DataTypes.INTEGER,
-      allowNull: true
-    },
-    priceTo: {
-      type: DataTypes.INTEGER,
-      allowNull: true
-    },
-    scanfilePath: {
-      type: DataTypes.STRING,
-      allowNull: true
-    },
-    stage: {
-      type: DataTypes.STRING,
-      allowNull: true
-    },
-    state: {
-      type: DataTypes.STRING,
-      allowNull: true
-    },
-    streetName: {
-      type: DataTypes.INTEGER,
-      allowNull: true
-    },
-    suburb: {
-      type: DataTypes.STRING,
-      allowNull: true
-    },
-    surname: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    telephone1: {
-      type: DataTypes.INTEGER,
-      allowNull: true
-    },
-    telephone2: {
-      type: DataTypes.INTEGER,
-      allowNull: true
-    },
-    telephone3: {
-      type: DataTypes.INTEGER,
-      allowNull: true
-    }
-  }, {
-    createdAt: 'dateTimeCreated',
-    updatedAt: 'dateTimeModified',
-    indexes: [
-      {
+  const Buyer = sequelize.define(
+    'Buyer',
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
         unique: true,
-        fields: ['id']
+        allowNull: false
+      },
+      region: {
+        type: DataTypes.STRING,
+        allowNull: true
+      },
+      buyerNotes: {
+        type: DataTypes.STRING,
+        allowNull: true
+      },
+      smSent: {
+        type: DataTypes.STRING,
+        allowNull: true
+      },
+      smSentDate: {
+        type: DataTypes.DATE,
+        allowNull: true
+      },
+      active: {
+        type: DataTypes.STRING,
+        allowNull: true
+      },
+      address2: {
+        type: DataTypes.STRING,
+        allowNull: true
+      },
+      businessIndustry: {
+        type: DataTypes.STRING,
+        allowNull: true
+      },
+      businessType: {
+        type: DataTypes.STRING,
+        allowNull: true
+      },
+      buyerSource: {
+        type: DataTypes.STRING,
+        allowNull: true
+      },
+      caReceived: {
+        type: DataTypes.STRING,
+        allowNull: true
+      },
+      caSent: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      checkAddress: {
+        type: DataTypes.STRING,
+        allowNull: true
+      },
+      checkEmail: {
+        type: DataTypes.STRING,
+        allowNull: true
+      },
+      country: {
+        type: DataTypes.STRING,
+        allowNull: true
+      },
+      displayName: {
+        type: DataTypes.STRING,
+        allowNull: true
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      emailOptional: {
+        type: DataTypes.STRING,
+        allowNull: true
+      },
+      fax: {
+        type: DataTypes.INTEGER,
+        allowNull: true
+      },
+      firstName: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      mailingAddress: {
+        type: DataTypes.INTEGER,
+        allowNull: true
+      },
+      newsletter: {
+        type: DataTypes.STRING,
+        allowNull: true
+      },
+      ownersTime: {
+        type: DataTypes.STRING,
+        allowNull: true
+      },
+      postCode: {
+        type: DataTypes.INTEGER,
+        allowNull: true
+      },
+      priceFrom: {
+        type: DataTypes.INTEGER,
+        allowNull: true
+      },
+      priceTo: {
+        type: DataTypes.INTEGER,
+        allowNull: true
+      },
+      scanfilePath: {
+        type: DataTypes.STRING,
+        allowNull: true
+      },
+      stage: {
+        type: DataTypes.STRING,
+        allowNull: true
+      },
+      state: {
+        type: DataTypes.STRING,
+        allowNull: true
+      },
+      streetName: {
+        type: DataTypes.INTEGER,
+        allowNull: true
+      },
+      suburb: {
+        type: DataTypes.STRING,
+        allowNull: true
+      },
+      surname: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      telephone1: {
+        type: DataTypes.INTEGER,
+        allowNull: true
+      },
+      telephone2: {
+        type: DataTypes.INTEGER,
+        allowNull: true
+      },
+      telephone3: {
+        type: DataTypes.INTEGER,
+        allowNull: true
       }
-    ]
-  })
+    },
+    {
+      createdAt: 'dateTimeCreated',
+      updatedAt: 'dateTimeModified',
+      indexes: [
+        {
+          unique: true,
+          fields: ['id']
+        }
+      ]
+    }
+  )
 
   return Buyer
 }
