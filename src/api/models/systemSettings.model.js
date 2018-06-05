a5bca7edf10bf6286f30b5a5c0d74bea7fec6305
Cayo Bayestorff <bayesrox@gmail.com>
2018-06-05 23:05:07 +1000

    created table systemSettings

---
+++ src/api/models/systemSettings.model.js
@@ -0,0 +1,30 @@
+export default (sequelize, DataTypes) => {
+  const SystemSettings = sequelize.define(
+    'SystemSettings',
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+        unique: true,
+        allowNull: true
+      },
+      emailOffice: {
+        type: DataTypes.STRING,
+        allowNull: true
+      }
+    },
+    {
+      createdAt: 'dateTimeCreated',
+      updatedAt: 'dateTimeModified',
+      indexes: [
+        {
+          unique: true,
+          fields: ['id']
+        }
+      ]
+    }
+  )
+
+  return SystemSettings
+}
