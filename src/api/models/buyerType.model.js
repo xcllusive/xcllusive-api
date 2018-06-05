571ad032972ab60eaed3448530cdef6b07fd548f
Cayo Bayestorff <bayesrox@gmail.com>
2018-06-05 10:47:56 +1000

    created Buyer Type

---
+++ src/api/models/buyerType.model.js
@@ -0,0 +1,30 @@
+export default (sequelize, DataTypes) => {
+  const BuyerType = sequelize.define(
+    'BuyerType',
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+        unique: true,
+        allowNull: true
+      },
+      label: {
+        type: DataTypes.STRING,
+        allowNull: true
+      }
+    },
+    {
+      createdAt: 'dateTimeCreated',
+      updatedAt: 'dateTimeModified',
+      indexes: [
+        {
+          unique: true,
+          fields: ['id']
+        }
+      ]
+    }
+  )
+
+  return BuyerType
+}
