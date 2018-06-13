07cb29995c9cd4a0a868209282db8c8b1d1eedcf
Bruno da Silva Valenga <brunodasilvalenga@gmail.com>
2018-06-13 00:31:13 -0300

    fixed break lines

--- src/api/utils/sharedFunctionsObject.js
+++ src/api/utils/sharedFunctionsObject.js
@@ -1,9 +1,9 @@
 export const transformQueryAndCleanNull = obj => {
   Object.keys(obj).forEach(prop => {
     let value = obj[prop]
-    if (value !== null && typeof value === 'object')
+    if (value !== null && typeof value === 'object') {
       obj[prop] = transformQueryAndCleanNull(value)
-    else if (value === null) {
+    } else if (value === null) {
       // Yes currently i use uppercase null for this, maybe change it to lowercase
       obj[prop] = ''
     }
