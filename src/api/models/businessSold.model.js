963463e0786198f3d0e60299f638aa43f261c708
Cayo Bayestorff <cayobayestorff@Cayos-iMac.local>
2018-09-24 13:08:58 +1000

    created businessModel

---
+++ src/api/models/businessSold.model.js
@@ -0,0 +1,112 @@
+export default (sequelize, DataTypes) => {
+  const BusinessSold = sequelize.define(
+    'BusinessSold',
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+        unique: true
+      },
+      businessType: {
+        type: DataTypes.STRING,
+        allowNull: true
+      },
+      settlementDate: {
+        type: DataTypes.DATE,
+        allowNull: true
+      },
+      buyerName: {
+        type: DataTypes.STRING,
+        allowNull: true
+      },
+      soldPrice: {
+        type: DataTypes.INTEGER,
+        allowNull: true
+      },
+      stockValue: {
+        type: DataTypes.INTEGER,
+        allowNull: true
+      },
+      assetValue: {
+        type: DataTypes.INTEGER,
+        allowNull: true
+      },
+      workingCapitalReq: {
+        type: DataTypes.INTEGER,
+        allowNull: true
+      },
+      propertyValue: {
+        type: DataTypes.INTEGER,
+        allowNull: true
+      },
+      year1: {
+        type: DataTypes.INTEGER,
+        allowNull: true
+      },
+      year2: {
+        type: DataTypes.INTEGER,
+        allowNull: true
+      },
+      year3: {
+        type: DataTypes.INTEGER,
+        allowNull: true
+      },
+      year4: {
+        type: DataTypes.INTEGER,
+        allowNull: true
+      },
+      nOfWorkingOwners: {
+        type: DataTypes.INTEGER,
+        allowNull: true
+      },
+      agreedWageForWorkingOwners: {
+        type: DataTypes.INTEGER,
+        allowNull: true
+      },
+      latestFullYearTotalRevenue: {
+        type: DataTypes.INTEGER,
+        allowNull: true
+      },
+      termsOfDeal: {
+        type: DataTypes.TEXT,
+        allowNull: true
+      },
+      specialNotes: {
+        type: DataTypes.INTEGER,
+        allowNull: true
+      },
+      sold: {
+        type: DataTypes.BOOLEAN,
+        allowNull: true,
+        defaultValue: false
+      }
+    },
+    {
+      createdAt: 'dateTimeCreated',
+      updatedAt: 'dateTimeModified',
+      indexes: [
+        {
+          unique: true,
+          fields: ['id']
+        }
+      ]
+    }
+  )
+
+  BusinessSold.associate = models => {
+    models.BusinessSold.belongsTo(models.User, {
+      foreignKey: 'createdBy_id',
+      as: 'CreatedBy'
+    })
+    models.BusinessSold.belongsTo(models.User, {
+      foreignKey: 'modifiedBy_id',
+      as: 'ModifiedBy'
+    })
+    models.BusinessSold.belongsTo(models.Business, {
+      foreignKey: 'business_id'
+    })
+  }
+
+  return BusinessSold
+}
