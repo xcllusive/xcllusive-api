b4781f9e96bf305c1ff3cc18fa2050a5ddb17645
Bruno da Silva Valenga <brunodasilvalenga@gmail.com>
2018-11-26 22:35:07 -0200

    v1.0.5

--- src/config/express.js
+++ src/config/express.js
@@ -13,17 +13,22 @@ import routes from '../api/routes'
 import { logs } from './vars'
 import error from '../api/middlewares/error'
 
+const corsOptions = {
+  origin: '*',
+  optionsSuccessStatus: 200
+}
+
 const app = express()
 
 app.use(morgan(logs))
 app.use(busboy())
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }))
+app.use(bodyParser.json({ limit: '50mb' }))
 app.use(busboyBodyParser())
 app.use(compress())
 app.use(methodOverride())
 app.use(helmet())
-app.use(cors())
+app.use(cors(corsOptions))
 app.use(paginate.middleware(10, 50))
 app.use('/', routes)
 app.use(error.converter)
