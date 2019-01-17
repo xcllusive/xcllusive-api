6f6bd07a3b4b26dd414435c697b4298359573aaf
Bruno da Silva Valenga <brunodasilvalenga@gmail.com>
2019-01-17 00:08:30 -0200

    update new appraisal

--- src/api/controllers/appraisal.controller.js
+++ src/api/controllers/appraisal.controller.js
@@ -3,6 +3,7 @@ import fs from 'fs'
 import path from 'path'
 import handlebars from 'handlebars'
 import puppeteer from 'puppeteer'
+import moment from 'moment'
 import models from '../../config/sequelize'
 import APIError from '../utils/APIError'
 // import mailer from '../modules/mailer'
@@ -143,7 +144,108 @@ export const generatePdf = async (req, res, next) => {
       })
     }
 
-    const context = {}
+    const context = {
+      currentYear: moment().get('year'),
+      businessName: appraisal.Business.businessName,
+      dateTimeModified: appraisal.dateTimeModified,
+      businessABN: appraisal.Business.businessABN,
+      businessAddress: appraisal.Business.address1,
+      businessSuburb: appraisal.Business.suburb,
+      businessState: appraisal.Business.state,
+      businessPostCode: appraisal.Business.postCode,
+      businessFirstNameV: appraisal.Business.firstNameV,
+      businessLastNameV: appraisal.Business.lastNameV,
+      businessIndustry: appraisal.Business.businessIndustry,
+      businessType: appraisal.Business.businessType,
+      currentOwner: appraisal.currentOwner,
+      nOfBusinessLocations: appraisal.nOfBusinessLocations,
+      businessCommenced: appraisal.businessCommenced,
+      tradingHours: appraisal.tradingHours,
+      productsServices: appraisal.productsServices,
+      descriptionCustomers: appraisal.descriptionCustomers,
+      clientDatabaseAvailable: appraisal.clientDatabaseAvailable ? 'Yes' : 'No',
+      client10TO: appraisal.client10TO ? 'Yes' : 'No',
+      client5TO: appraisal.client5TO,
+      descriptionSuppliers: appraisal.descriptionSuppliers,
+      premisesOwnedRented: appraisal.premisesOwnedRented,
+      rentCost: appraisal.rentCost,
+      timeRemLease: appraisal.timeRemLease,
+      premisesNotes: appraisal.premisesNotes,
+      fullTime: appraisal.fullTime,
+      partTime: appraisal.partTime,
+      subContractors: appraisal.subContractors,
+      casuals: appraisal.casuals,
+      numberOwners: appraisal.numberOwners,
+      owners1sHours: appraisal.owners1sHours,
+      owners1sRole: appraisal.owners1sRole,
+      otherOwnersHours: appraisal.otherOwnersHours,
+      otherOwnersRole: appraisal.otherOwnersRole,
+      otherRelevantNotes: appraisal.otherRelevantNotes,
+      year1: appraisal.year1,
+      year2: appraisal.year2,
+      year3: appraisal.year3,
+      year4: appraisal.year4,
+      year5: appraisal.year5,
+      year6: appraisal.year6,
+      year7: appraisal.year7,
+      sales1: appraisal.sales1,
+      cogs1: appraisal.cogs1,
+      grossMargin1: appraisal.grossMargin1,
+      grossMarginPerc1: appraisal.grossMarginPerc1,
+      otherIncome1: appraisal.otherIncome1,
+      grossProfit1: appraisal.grossProfit1,
+      expenses1: appraisal.expenses1,
+      operatingProfit1: appraisal.operatingProfit1,
+      operatingProfitPerc1: appraisal.operatingProfitPerc1,
+      cogs2: appraisal.cogs2,
+      grossMargin2: appraisal.grossMargin2,
+      grossMarginPerc2: appraisal.grossMarginPerc2,
+      otherIncome2: appraisal.otherIncome2,
+      grossProfit2: appraisal.grossProfit2,
+      expenses2: appraisal.expenses2,
+      operatingProfit2: appraisal.operatingProfit2,
+      operatingProfitPerc2: appraisal.operatingProfitPerc2,
+      cogs3: appraisal.cogs3,
+      grossMargin3: appraisal.grossMargin3,
+      grossMarginPerc3: appraisal.grossMarginPerc3,
+      otherIncome3: appraisal.otherIncome3,
+      grossProfit3: appraisal.grossProfit3,
+      expenses3: appraisal.expenses3,
+      operatingProfit3: appraisal.operatingProfit3,
+      operatingProfitPerc3: appraisal.operatingProfitPerc3,
+      cogs4: appraisal.cogs4,
+      grossMargin4: appraisal.grossMargin4,
+      grossMarginPerc4: appraisal.grossMarginPerc4,
+      otherIncome4: appraisal.otherIncome4,
+      grossProfit4: appraisal.grossProfit4,
+      expenses4: appraisal.expenses4,
+      operatingProfit4: appraisal.operatingProfit4,
+      operatingProfitPerc4: appraisal.operatingProfitPerc4,
+      cogs5: appraisal.cogs5,
+      grossMargin5: appraisal.grossMargin5,
+      grossMarginPerc5: appraisal.grossMarginPerc5,
+      otherIncome5: appraisal.otherIncome5,
+      grossProfit5: appraisal.grossProfit5,
+      expenses5: appraisal.expenses5,
+      operatingProfit5: appraisal.operatingProfit5,
+      operatingProfitPerc5: appraisal.operatingProfitPerc5,
+      cogs6: appraisal.cogs6,
+      grossMargin6: appraisal.grossMargin6,
+      grossMarginPerc6: appraisal.grossMarginPerc6,
+      otherIncome6: appraisal.otherIncome6,
+      grossProfit6: appraisal.grossProfit6,
+      expenses6: appraisal.expenses6,
+      operatingProfit6: appraisal.operatingProfit6,
+      operatingProfitPerc6: appraisal.operatingProfitPerc6,
+      cogs7: appraisal.cogs7,
+      grossMargin7: appraisal.grossMargin7,
+      grossMarginPerc7: appraisal.grossMarginPerc7,
+      otherIncome7: appraisal.otherIncome7,
+      grossProfit7: appraisal.grossProfit7,
+      expenses7: appraisal.expenses7,
+      operatingProfit7: appraisal.operatingProfit7,
+      operatingProfitPerc7: appraisal.operatingProfitPerc7
+    }
 
     const PDF_OPTIONS = {
       path: destPdfGenerated,
@@ -155,14 +257,14 @@ export const generatePdf = async (req, res, next) => {
         right: '15mm',
         bottom: '15mm'
       },
-      displayHeaderFooter: false,
+      displayHeaderFooter: true,
       headerTemplate: ' ',
       footerTemplate: `
       <div style="margin-left:15mm;margin-right:15mm;width:100%;font-size:12px;text-align:center;color:rgb(187, 187, 187);">
-      <span style="float: left;"></span>
+      <span style="float: left;">Sales Inspection Report and Business Appraisal for NAME OF BUSINEES HERE</span>
       <span style="float: right;">Page: <span class="pageNumber"></span> of <span class="totalPages"></span></span>
-      </div>`,
-      scale: 0.8
+      </div>`
+      // scale: 0.8
     }
 
     const content = await readFile(templatePath, 'utf8')
