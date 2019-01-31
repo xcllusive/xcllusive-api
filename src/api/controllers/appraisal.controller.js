c73dc6ad6aea18c566e7b9cbf7512b9c257ef86b
Cayo Bayestorff <cayobayestorff@Cayos-iMac.local>
2019-01-31 17:04:14 +1100

    changes

--- src/api/controllers/appraisal.controller.js
+++ src/api/controllers/appraisal.controller.js
@@ -16,14 +16,22 @@ import {
 // import mailer from '../modules/mailer'
 
 export const list = async (req, res, next) => {
-  const { businessId } = req.query
+  const {
+    businessId
+  } = req.query
 
   const limit = req.query.limit
   const offset = req.skip
-  const where = businessId ? { business_id: businessId } : null
+  const where = businessId ? {
+    business_id: businessId
+  } : null
 
   try {
-    const response = await models.Appraisal.findAndCountAll({ where, limit, offset })
+    const response = await models.Appraisal.findAndCountAll({
+      where,
+      limit,
+      offset
+    })
     return res.status(201).json({
       data: response,
       pageCount: response.count,
@@ -35,15 +43,26 @@ export const list = async (req, res, next) => {
 }
 
 export const get = async (req, res, next) => {
-  const { appraisalId } = req.params
+  const {
+    appraisalId
+  } = req.params
 
   try {
     const response = await models.Appraisal.findOne({
-      where: { id: appraisalId },
-      include: [
-        { model: models.Business },
-        { model: models.User, as: 'CreatedBy' },
-        { model: models.User, as: 'ModifiedBy' }
+      where: {
+        id: appraisalId
+      },
+      include: [{
+          model: models.Business
+        },
+        {
+          model: models.User,
+          as: 'CreatedBy'
+        },
+        {
+          model: models.User,
+          as: 'ModifiedBy'
+        }
       ]
     })
     return res.status(201).json({
@@ -62,14 +81,18 @@ export const create = async (req, res, next) => {
   try {
     // Verify exists appraisal to change the stage to appraisal if you do not have any
     const haveAppraisal = await models.Appraisal.findOne({
-      where: { business_id: newAppraisal.business_id }
+      where: {
+        business_id: newAppraisal.business_id
+      }
     })
     if (!haveAppraisal) {
       const updateBusiness = req.body
       updateBusiness.modifiedBy_id = req.user.id
       updateBusiness.stageId = 9
       await models.Business.update(updateBusiness, {
-        where: { id: newAppraisal.business_id }
+        where: {
+          id: newAppraisal.business_id
+        }
       })
     }
 
@@ -77,35 +100,54 @@ export const create = async (req, res, next) => {
 
     return res
       .status(200)
-      .json({ data: appraisal, message: `Appraisal ${appraisal.id} created` })
+      .json({
+        data: appraisal,
+        message: `Appraisal ${appraisal.id} created`
+      })
   } catch (error) {
     return next(error)
   }
 }
 
 export const update = async (req, res, next) => {
-  const { appraisalId } = req.params
+  const {
+    appraisalId
+  } = req.params
   const updatedValues = req.body
 
   updatedValues.updatedBy_id = req.user.id
 
   try {
-    await models.Appraisal.update(updatedValues, { where: { id: appraisalId } })
-    return res.status(200).json({ message: `Appraisal ${appraisalId} updated` })
+    await models.Appraisal.update(updatedValues, {
+      where: {
+        id: appraisalId
+      }
+    })
+    return res.status(200).json({
+      message: `Appraisal ${appraisalId} updated`
+    })
   } catch (error) {
     return next(error)
   }
 }
 
 export const remove = async (req, res, next) => {
-  const { appraisalId } = req.params
+  const {
+    appraisalId
+  } = req.params
 
   try {
-    await models.Appraisal.destroy({ where: { id: appraisalId } })
+    await models.Appraisal.destroy({
+      where: {
+        id: appraisalId
+      }
+    })
 
     return res
       .status(200)
-      .json({ message: `Appraisal ${appraisalId} removed with success` })
+      .json({
+        message: `Appraisal ${appraisalId} removed with success`
+      })
   } catch (error) {
     return next(error)
   }
@@ -136,7 +178,9 @@ export const remove = async (req, res, next) => {
 // }
 
 export const generatePdf = async (req, res, next) => {
-  const { appraisalId } = req.params
+  const {
+    appraisalId
+  } = req.params
 
   const templatePath = path.resolve(
     'src',
@@ -163,8 +207,13 @@ export const generatePdf = async (req, res, next) => {
   try {
     // Verify exists appraisal
     const appraisal = await models.Appraisal.findOne({
-      where: { id: appraisalId },
-      include: [{ model: models.Business, as: 'Business' }]
+      where: {
+        id: appraisalId
+      },
+      include: [{
+        model: models.Business,
+        as: 'Business'
+      }]
     })
 
     if (!appraisal) {
@@ -176,7 +225,9 @@ export const generatePdf = async (req, res, next) => {
     }
 
     const businessType = await models.BusinessType.findOne({
-      where: { id: appraisal.Business.typeId }
+      where: {
+        id: appraisal.Business.typeId
+      }
     })
 
     const businessSoldselectedListOnlyId = JSON.parse(
@@ -184,7 +235,9 @@ export const generatePdf = async (req, res, next) => {
     )
 
     const comparableDataSelectedList = await models.BusinessSold.findAll({
-      where: { id: Array.from(businessSoldselectedListOnlyId) }
+      where: {
+        id: Array.from(businessSoldselectedListOnlyId)
+      }
     })
 
     const variables = {
@@ -205,7 +258,9 @@ export const generatePdf = async (req, res, next) => {
       financialInformationArray: []
     }
 
-    const context = Object.assign(appraisal.get({ plain: true }), variables)
+    const context = Object.assign(appraisal.get({
+      plain: true
+    }), variables)
 
     /* profits table */
 
@@ -293,7 +348,7 @@ export const generatePdf = async (req, res, next) => {
         context.multiplierLabel = 'Multiplier PEBITDA Avg With Stock'
         multiplier = numeral(
           item.soldPrice /
-            (ebitdaAvg(item) + item.agreedWageForMainOwner + item.stockValue)
+          (ebitdaAvg(item) + item.agreedWageForMainOwner + item.stockValue)
         ).format('0,0.[99]')
       }
       if (appraisal.pricingMethod === 9) {
@@ -344,6 +399,26 @@ export const generatePdf = async (req, res, next) => {
     })
     /* end table 10 last businesses */
     context.avgMultiplier = numeral(totalMultiplier / totalIndex).format('0,0.[99]')
+
+    // start gauges
+    context.valueSliderBR = appraisal.valueSliderBR
+    context.valueSliderMarket = appraisal.valueSliderMarket
+
+    const descriptionBusinessRisk = await models.AppraisalRegister.findOne({
+      where: {
+        points: appraisal.valueSliderBR,
+        type: 'descriptionBusinessRisk'
+      }
+    })
+    const descriptionMarket = await models.AppraisalRegister.findOne({
+      where: {
+        points: appraisal.valueSliderMarket,
+        type: 'descriptionMarket'
+      }
+    })
+    context.descriptionGaugeBusinessRisk = descriptionBusinessRisk.label
+    context.descriptionGaugeMarket = descriptionMarket.label
+    // end gauges
 
     // start table financial information
     for (let i = 1; i < 31; i++) {
