c5b88064e0d95c21d6f51b0907917bcd05bc14ce
Bruno da Silva Valenga <brunodasilvalenga@gmail.com>
2019-11-02 02:36:19 +1100

    testing removing dotenv-safe

--- src/config/vars.js
+++ src/config/vars.js
@@ -1,10 +1,10 @@
-import path from 'path'
+// import path from 'path'
 
-if (process.env.NODE_ENV === 'development') {
-  require('dotenv-safe').load({
-    path: path.join(__dirname, '../../.env')
-  })
-}
+// if (process.env.NODE_ENV === 'development') {
+//   require('dotenv-safe').load({
+//     path: path.join(__dirname, '../../.env')
+//   })
+// }
 
 module.exports = {
   env: process.env.NODE_ENV,
