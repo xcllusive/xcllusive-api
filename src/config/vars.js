1bd8d92fbe4e8513f9f445db74d2fcf6ccd789e9
Bruno da Silva Valenga <brunodasilvalenga@gmail.com>
2019-11-07 20:45:10 +1100

    create script to run dev local

--- src/config/vars.js
+++ src/config/vars.js
@@ -12,10 +12,10 @@ module.exports = {
   jwtSecret: process.env.JWT_SECRET,
   jwtExpirationInterval: process.env.JWT_EXPIRATION_HOURS,
   mysql: {
-    uri: process.env.NODE_ENV === 'development' ? process.env.MYSQL_URI_DEV : process.env.MYSQL_URI,
-    database: process.env.NODE_ENV === 'development' ? process.env.MYSQL_DATABASE_DEV : process.env.MYSQL_DATABASE,
-    user: process.env.NODE_ENV === 'development' ? process.env.MYSQL_USERNAME_DEV : process.env.MYSQL_USERNAME,
-    password: process.env.NODE_ENV === 'development' ? process.env.MYSQL_PASSWORD_DEV : process.env.MYSQL_PASSWORD
+    uri: process.env.NODE_ENV === 'development' || process.env.NODE_ENV === 'development-local' ? process.env.MYSQL_URI_DEV : process.env.MYSQL_URI,
+    database: process.env.NODE_ENV === 'development' || process.env.NODE_ENV === 'development-local' ? process.env.MYSQL_DATABASE_DEV : process.env.MYSQL_DATABASE,
+    user: process.env.NODE_ENV === 'development' || process.env.NODE_ENV === 'development-local' ? process.env.MYSQL_USERNAME_DEV : process.env.MYSQL_USERNAME,
+    password: process.env.NODE_ENV === 'development' || process.env.NODE_ENV === 'development-local' ? process.env.MYSQL_PASSWORD_DEV : process.env.MYSQL_PASSWORD
   },
   mail: {
     host: process.env.EMAIL_HOST,
